0aee8bdc432d5f10df9b895225f6c82b2c0b282f
unknown <mm5213@eews506a-047.ic.ac.uk>
2015-11-10 12:32:16 +0000

    fixed with nickbae

--- cw2.sql
+++ cw2.sql
@@ -1,26 +1,25 @@
 -- Q1 returns (state_name)
 
-SELECT  feature.state_name 
-FROM feature LEFT JOIN state USING(name)
-WHERE code IS NOT NULL
-;
+--List in alphabetical order as the scheme (state name) those state names that appear feature
+--, but not in state. Note that you must ignore case in comparing state names, so that
+--‘ARIZONA’ will be treated as equivalent to ‘Arizona’.
 
---or
-
-
-SELECT UPPER(state_name) AS state_name
+SELECT DISTINCT UPPER(state_name) AS state_name
 FROM feature
 EXCEPT
-SELECT UPPER(name) AS state_name
+SELECT DISTINCT UPPER(name) AS state_name
 FROM state
 ORDER BY state_name
 ;
+
 --could be done better as a join??
 
 -- Q2 returns (state_name,county,no_ppl,no_stream)
+
 -- List in alphabetical order as the scheme (state name,county,no ppl,no stream) the number of
 -- populated places and the number of streams recorded in feature in each county. The list
 -- should be limited to those states that have state code between 1 and 9.
+
 SELECT 
 	feature.state_name, 
 	feature.county, 
@@ -32,32 +31,45 @@ AND state.code BETWEEN 1 AND 9
 AND feature.county IS NOT NULL
 GROUP BY feature.state_name, feature.county
 ORDER BY feature.state_name, feature.county
-
 ;
 
 -- Q3 returns (state_abbr,name,latitude,longitude,elevation)
-SELECT state.abbr AS abbr, populated_place.name, latitude,longitude,elevation
+
+--Populated places are found in both the feature and populated place tables. Write a query
+--that returns the scheme (state abbr,name,latitude,longitude,elevation) listing in alphabetical
+--order the state abbreviation and populated places name of all populated places above 10,000
+--feet elevation. If, due to inconsistency in the data, two records for the same place disagree
+--in any of latitude, longitude, or elevation, then both records should be listed.
+
+(SELECT state.abbr AS state_abbr, populated_place.name, latitude,longitude,elevation
 FROM populated_place JOIN state 
 ON state.code = state_code
 AND elevation > 10000
 
 UNION 
 
-SELECT state.abbr AS abbr, feature.name, latitude,longitude,elevation
+SELECT state.abbr AS state_abbr, feature.name, latitude,longitude,elevation
 FROM feature JOIN state 
 ON UPPER(state.name) = UPPER(feature.state_name)
 AND type = 'ppl'
-AND elevation > 10000 
-ORDER BY abbr, name;
+AND elevation > 10000 )
+ORDER BY state_abbr, name;
 
 -- Q4 returns (state_name,county)
--- List in alphabetical order the scheme (state_name,county) listing the names of states,
--- and counties of those states found in populated_place, for which all entries of the county 
--- have an unknown population.
+
+-- List in alphabetical order the scheme (state_name,county) listing the names of states, 
+-- and counties of those states found in populated_place, for which all entries of the 
+-- county have an unknown population.
+
 SELECT state.name AS state_name, county
 FROM populated_place JOIN state ON populated_place.state_code = state.code
-GROUP BY county, state.name
-HAVING SUM(CASE WHEN population IS NOT NULL THEN 1 ELSE 0 END) = 0
+WHERE county NOT IN (
+					SELECT county
+					FROM populated_place
+					WHERE population IS NOT NULL
+					)
+--GROUP BY county, state.name
+--HAVING SUM(CASE WHEN population IS NOT NULL THEN 1 ELSE 0 END) = 0
 ORDER BY state.name, county
 ;
 
@@ -70,13 +82,14 @@ ORDER BY state.name, county
 --The query result must be ordered by the state name, county and cell name. The query result
 --should also exclude listing cells for which there is no information about the population, and
 --the list should be limited to those states that have state code between 1 and 9.
-SELECT 
+
+SELECT DISTINCT
 	state.name AS state_name,
 	county, 
 	cell_name, 
 	SUM(population) OVER (PARTITION BY cell_name)AS population,
 	SUM(population) OVER (PARTITION BY county)AS county_population,
-	SUM(population) OVER (PARTITION BY state)AS state_population
+	SUM(population) OVER (PARTITION BY state.name)AS state_population
 FROM populated_place JOIN state 
 ON state.code = populated_place.state_code
 AND populated_place.state_code BETWEEN 1 AND 9
@@ -94,9 +107,11 @@ ORDER BY state_name, county, cell_name
 --query should assume that the earth is a perfect sphere of radius 3959 miles, and round the
 --distance figure to two decimal places
 
-SELECT populated_place.name AS populated_place_name, feature.name AS feature_name, 
-ROUND((3959 * ACOS(COS(RADIANS(feature.latitude)) * COS(RADIANS(populated_place.latitude)) * COS( RADIANS(populated_place.longitude) - RADIANS(feature.longitude)) + SIN( RADIANS(feature.latitude)) * SIN(RADIANS(populated_place.latitude))))::numeric, 2) AS distance,
-DENSE_RANK() OVER (PARTITION BY populated_place.name ORDER BY ROUND(3959 * ACOS(COS(RADIANS(feature.latitude)) * COS(RADIANS(populated_place.latitude)) * COS( RADIANS(populated_place.longitude) - RADIANS(feature.longitude)) + SIN( RADIANS(feature.latitude)) * SIN(RADIANS(populated_place.latitude))))::numeric ASC) AS rank
+SELECT 
+	populated_place.name AS populated_place_name, 
+	feature.name AS feature_name, 
+	ROUND((3959 * ACOS(COS(RADIANS(feature.latitude)) * COS(RADIANS(populated_place.latitude)) * COS( RADIANS(populated_place.longitude) - RADIANS(feature.longitude)) + SIN( RADIANS(feature.latitude)) * SIN(RADIANS(populated_place.latitude))))::numeric, 2) AS distance,
+	DENSE_RANK() OVER (PARTITION BY populated_place.name ORDER BY ROUND(3959 * ACOS(COS(RADIANS(feature.latitude)) * COS(RADIANS(populated_place.latitude)) * COS( RADIANS(populated_place.longitude) - RADIANS(feature.longitude)) + SIN( RADIANS(feature.latitude)) * SIN(RADIANS(populated_place.latitude))))::numeric ASC) AS rank
 FROM populated_place JOIN feature
 ON feature.type = 'summit'
 AND population >= 100000
